fc6a5677ded13833cf737b424d8d39ba7b368670
Ayman Mustafa <84941140+MEClouds@users.noreply.github.com>
2025-10-17 20:35:35 +0100

    04: integrate authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,18 @@
 import { Button } from "@/components/ui/button"
-import { getQueryClient, trpc } from "@/trpc/server"
-import { Client } from "./client"
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
-import { Suspense } from "react"
+import { requireAuth } from "@/lib/auth-utils"
+import { caller } from "@/trpc/server"
+import { LogoutButton } from "./logout"
 
 const Page = async () => {
-  const queryClient = getQueryClient()
-
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
-
+  // requireAuth just for user experience not for security
+  await requireAuth()
+  //data access layer with protected procedure from tRPC
+  const data = await caller.getUsers()
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      Integrate TRPC
-      <Button className="m-4" variant={"outline"} size={"sm"}>
-        Done
-      </Button>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>loading...</p>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+    <div className="min-h-screen min-w-screen flex flex-col gap-y-5 items-center  justify-center">
+      Integrate Better Auth [ Protected Server Component]
+      <div>{JSON.stringify(data, null, 2)}</div>
+      <LogoutButton />
     </div>
   )
 }
