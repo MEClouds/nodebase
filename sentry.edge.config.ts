54035e350a2c6276d85feee182fe6188b49b2a40
Ayman Mustafa <84941140+MEClouds@users.noreply.github.com>
2025-10-22 22:30:15 +0100

    09: sidebar layout

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -3,14 +3,14 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs"
 
 Sentry.init({
   dsn: "https://e4f14ae66b03d33aa75a3ec3daa1c7d4@o4510229592276992.ingest.de.sentry.io/4510229612593232",
-  
-  integrations:[
+
+  integrations: [
     Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
-  ]
+  ],
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
@@ -20,4 +20,4 @@ Sentry.init({
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
-});
+})
