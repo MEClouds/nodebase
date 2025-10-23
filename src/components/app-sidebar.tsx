699b0525af13e70eaa92c06c8e8550a39707e69e
Ayman Mustafa <84941140+MEClouds@users.noreply.github.com>
2025-10-23 22:59:13 +0100

    10: integrate Polar payments

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -24,6 +24,7 @@ import {
   SidebarMenuItem,
 } from "./ui/sidebar"
 import { authClient } from "@/lib/auth-client"
+import { useHasActiveSubscription } from "./payment/hooks/use-subscription"
 
 const menuItems = [
   {
@@ -51,6 +52,7 @@ const menuItems = [
 export const AppSidebar = () => {
   const router = useRouter()
   const pathname = usePathname()
+  const { hasActiveSubscription, isLoading } = useHasActiveSubscription()
   return (
     <Sidebar collapsible="icon">
       <SidebarHeader>
@@ -99,21 +101,23 @@ export const AppSidebar = () => {
 
       <SidebarFooter>
         <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton
-              tooltip="Upgrade to Pro"
-              className="gap-x-4 h-10 px-4"
-              onClick={() => {}}
-            >
-              <StarIcon className="h-4 w-4" />
-              <span>Upgrade Now</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
+          {!hasActiveSubscription && !isLoading && (
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                tooltip="Upgrade to Pro"
+                className="gap-x-4 h-10 px-4"
+                onClick={() => authClient.checkout({ slug: "pro" })}
+              >
+                <StarIcon className="h-4 w-4" />
+                <span>Upgrade Now</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          )}
           <SidebarMenuItem>
             <SidebarMenuButton
               tooltip="Billing Portal"
               className="gap-x-4 h-10 px-4"
-              onClick={() => {}}
+              onClick={() => authClient.customer.portal()}
             >
               <CreditCardIcon className="h-4 w-4" />
               <span>Billing Page</span>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
+import { checkout, polar, portal } from "@polar-sh/better-auth"
 import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
 import prisma from "@/lib/db"
-
+import { polarClient } from "./polar"
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
@@ -10,4 +11,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "84f4f1c4-5574-4fa4-8fbc-df28fcf1cc6a",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 })
