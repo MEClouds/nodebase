23b484a0212f99100516d629d683410bade1cc62
Ayman Mustafa <84941140+MEClouds@users.noreply.github.com>
2025-11-09 23:41:01 +0000

    13: workflows ui

--- src/app/(dashboard)/(main)/workflows/page.tsx
+++ src/app/(dashboard)/(main)/workflows/page.tsx
@@ -1,6 +1,8 @@
 import {
   WorkflowContainer,
+  WorkflowsError,
   WorkflowsList,
+  WorkflowsLoading,
 } from "@/features/workflows/components/workflows"
 import { workflowsParamsLoader } from "@/features/workflows/server/params-loader"
 import { prefetchWorkflows } from "@/features/workflows/server/prefetch"
@@ -20,8 +22,8 @@ const Page = async ({ searchParams }: Props) => {
   return (
     <WorkflowContainer>
       <HydrateClient>
-        <ErrorBoundary fallback={<p>Error</p>}>
-          <Suspense fallback={<p>loading...</p>}>
+        <ErrorBoundary fallback={<WorkflowsError />}>
+          <Suspense fallback={<WorkflowsLoading />}>
             <WorkflowsList />
           </Suspense>
         </ErrorBoundary>
